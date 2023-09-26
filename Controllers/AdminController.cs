5f4f159a09e9a651b05177bc6624d0443472ef40
Lộc Trần <loctt.it2k@gmail.com>
2023-09-26 23:45:10 +0700

    admin layout

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using KGQT.Business;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+
+namespace KGQT.Controllers
+{
+    public class AdminController : Controller
+    {
+        #region constructor
+        private IToastNotification _toastNotification;
+        public IToastNotification NotiService
+        {
+            get { return _toastNotification; }
+            set { _toastNotification = value; }
+        }
+
+        public AdminController(IToastNotification toastNotification)
+        {
+            NotiService = toastNotification;
+        }
+        
+        #endregion
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
