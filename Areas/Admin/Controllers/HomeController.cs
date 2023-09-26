a867ae8ff6aaa4373f221be01c03dc737ac87364
Lộc Trần <loctt.it2k@gmail.com>
2023-09-27 00:16:51 +0700

    admin/user layout

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -4,9 +4,13 @@ namespace KGQT.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        #region Index/Home
         public IActionResult Index()
         {
             return View();
         }
+
+        #endregion
+
     }
 }
