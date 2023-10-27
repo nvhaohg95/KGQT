7ebc7cf863d12398089cade35e341e2f7df2f80b
Lộc Trần <loctt.it2k@gmail.com>
2023-10-27 20:20:56 +0700

    loc push 27/10 list user

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using KGQT.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KGQT.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class UserController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var lst = UserBusiness.GetListUser();
+            return View(lst);
+        }
+
+        #region Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
