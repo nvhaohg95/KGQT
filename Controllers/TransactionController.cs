170f19588b0e15f4ddfc3269fad251385e9f2b3c
Lộc Trần <loctt.it2k@gmail.com>
2023-11-04 06:06:24 +0700

    4/11

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KGQT.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Nạp tiền
+        public IActionResult Recharge()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
