82f399c279ce5ef2f9ed95f2600ebf791c697252
tan <tmtan>
2024-04-18 12:16:22 +0700

    tan update git

--- Mobility/AuthencationController.cs
+++ Mobility/AuthencationController.cs
@@ -1,8 +1,12 @@
-﻿using KGQT.Business;
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using KGQT.Business;
 using KGQT.Business.Base;
 using KGQT.Commons;
 using KGQT.Models;
 using KGQT.Models.temp;
+using Microsoft.AspNetCore.Builder.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Drawing;
@@ -423,7 +427,7 @@ namespace KGQT.Mobility
                     if (!string.IsNullOrEmpty(token))
                     {
                         string body = $"Thanh toán cho đơn {oOrder?.ShippingOrderCode} thành công";
-                        //await SendFCMAsync(body, token, null);
+                        await SendFCMAsync(body, token, null);
                     }
                 }
 
@@ -541,6 +545,46 @@ namespace KGQT.Mobility
             if (model == null) return false;
             if (string.IsNullOrEmpty(model.DataRequest)) return false;
             return true;
+        }
+        private async Task<object> SendFCMAsync(string body, string token, string data)
+        {
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "Firebase-key\\" + "trakuaidi-app-firebase-key.json";
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile(path)
+                });
+            }
+
+            // This registration token comes from the client FCM SDKs.
+            //var registrationToken = "dLz14KR5QdmuioC_LZ0y8j:APA91bHduPYeNfEOAFHpOEhUmFAQrhZVvrhpok95mJulgQzF6OFYY99HukLTCAxNQ9lGpxhcdD0BE8WMqDKuVP2ahi4Z8wZT81qk8-3juViDjAa35oVWu650R90nMgPyOKAXL3D6GN-0";
+            // See documentation on defining a message payload.
+            var message = new Message()
+            {
+
+                Data = new Dictionary<string, string>()
+        {
+            { "data", data },
+        },
+                Token = token,
+                Android = new AndroidConfig()
+                {
+                    Notification = new AndroidNotification()
+                    {
+                        ChannelId = "Trakuaidi",
+                        Priority = NotificationPriority.HIGH,
+                        Title = "Trakuaidi xin thông báo",
+                        Body = body,
+                    },
+                }
+            };
+
+            // Send a message to the device corresponding to the provided
+            // registration token.
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            // Response is a message ID string.
+            return response;
         }
         #endregion
 
