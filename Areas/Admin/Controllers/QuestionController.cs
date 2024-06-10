a9c9d406697cf2cce3078317e09a31643d05fb03
Lộc Trần <loctt.it2k@gmail.com>
2024-06-10 23:15:13 +0700

    câu hỏi thường gặp & cộng điểm baidu

---
+++ Areas/Admin/Controllers/QuestionController.cs
@@ -0,0 +1,84 @@
+﻿using KGQT.Business;
+using KGQT.Commons;
+using KGQT.Models;
+using KGQT.Models.temp;
+using MailKit.Search;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace KGQT.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class QuestionController : Controller
+    {
+        public IActionResult Index(int page = 1, int pageSize = 10)
+        {
+            var oData = QuestionBusiness.GetList(page, pageSize);
+            var lstData = oData[0] as List<tbl_Questions>;
+            int numberRecord = (int)oData[1];
+            int numberPage = (int)oData[2];
+            ViewBag.pageCurrent = page;
+            ViewBag.numberPage = numberPage;
+            ViewBag.numberRecord = numberRecord;
+            return View(lstData);
+        }
+
+        public DataReturnModel<bool> Create(string question, string answer, int status)
+        {
+            DataReturnModel<bool> result = new();
+            var cookieService = new CookieService(HttpContext);
+            var tkck = cookieService.Get("tkck");
+            var userModel = JsonConvert.DeserializeObject<UserModel>(tkck);
+            string userLogin = userModel != null ? userModel.UserName : "";
+            if(!string.IsNullOrEmpty(userLogin))
+            {
+                result = QuestionBusiness.Insert(question, answer, status, userLogin);
+            }
+            else
+            {
+                result.IsError = true;
+                result.Message = "Hệ thống thực thi không thành công! Vui lòng thử lại";
+            }
+            return result;
+        }
+
+        public DataReturnModel<bool> Delete(int id) {
+            DataReturnModel<bool> result = new();
+            var cookieService = new CookieService(HttpContext);
+            var tkck = cookieService.Get("tkck");
+            var userModel = JsonConvert.DeserializeObject<UserModel>(tkck);
+            string userLogin = userModel != null ? userModel.UserName : "";
+            if (!string.IsNullOrEmpty(userLogin))
+            {
+                result = QuestionBusiness.Delete(id, userLogin);
+            }
+            else
+            {
+                result.IsError = true;
+                result.Message = "Hệ thống thực thi không thành công. Vui lòng thử lại!";
+            }
+            return result;
+        }
+
+        public DataReturnModel<bool> Update(tbl_Questions? data)
+        {
+            DataReturnModel<bool> result = new();
+            var cookieService = new CookieService(HttpContext);
+            var tkck = cookieService.Get("tkck");
+            var userModel = JsonConvert.DeserializeObject<UserModel>(tkck);
+            string userLogin = userModel != null ? userModel.UserName : "";
+            if (data != null)
+            {
+                result = QuestionBusiness.Update(data, userLogin);
+            }
+            return result;
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var result = QuestionBusiness.GetByID(id);
+            return View(result);
+        }
+
+    }
+}
---
+++ Business/QuestionBusiness.cs
@@ -0,0 +1,193 @@
+﻿using KGQT.Models;
+using KGQT.Models.temp;
+using System.Collections.Generic;
+using Serilog;
+using System.Text.RegularExpressions;
+using ILogger = Serilog.ILogger;
+namespace KGQT.Business
+{
+    public static class QuestionBusiness
+    {
+        private static readonly ILogger _log = Log.ForContext(typeof(AccountBusiness));
+        public static object[] GetList(int page, int pageSize)
+        {
+            using (var db = new nhanshiphangContext())
+            {
+                List<tbl_Questions> lstData = new();
+                int total = 0;
+                int totalPage = 0;
+                var query = db.tbl_Questions.Where(x => x.Status != 2);
+                total = query.Count();
+                if(total > 0)
+                {
+                    totalPage = Convert.ToInt32(Math.Ceiling((decimal)total / pageSize));
+                    int pageNum = (page - 1) * pageSize;
+                    lstData = query.Skip(pageNum).Take(pageSize).ToList();
+                }
+                return new object[] { lstData, total, totalPage };
+            }
+        }
+
+        public static DataReturnModel<bool> Insert(string question, string anwser, int status, string createdBy)
+        {
+            DataReturnModel<bool> result = new();
+            try
+            {
+                using (var db = new nhanshiphangContext())
+                {
+                    if (string.IsNullOrEmpty(question))
+                    {
+                        result.IsError = true;
+                        result.Message = "Câu hỏi không được bỏ trống!";
+                        return result;
+                    }
+                    if (string.IsNullOrEmpty(anwser))
+                    {
+                        result.IsError = true;
+                        result.Message = "Câu trả lời không được bỏ trống!";
+                        return result;
+                    }
+                    var admin = db.tbl_Accounts.FirstOrDefault(x => x.Username == createdBy.ToLower());
+                    if (admin != null)
+                    {
+                        tbl_Questions data = new tbl_Questions()
+                        {
+                            Question = question,
+                            Answer = anwser,
+                            Status = status,
+                            CreatedBy = admin.Username,
+                            CreatedOn = DateTime.Now,
+                        };
+                        db.Add(data);
+                        int kq = db.SaveChanges();
+                        if(kq > 0)
+                        {
+                            result.IsError = false;
+                            result.Data = true;
+                            result.Message = "Thêm thành công!";
+                            return result;
+                        }
+                    }
+                    result.IsError = true;
+                    result.Message = "Hệ thống thực thi không thành công. Vui lòng thử lại!";
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error("Lỗi tạo câu hỏi", ex.Message);
+                result.IsError = true;
+                result.Message = "Hệ thống thực thi không thành công. Vui lòng thử lại!";
+                return result;
+            }
+        }
+
+        public static DataReturnModel<bool> Update(tbl_Questions? dataUpdate, string createdBy)
+        {
+            DataReturnModel<bool> result = new();
+            try
+            {
+                using (var db = new nhanshiphangContext())
+                {
+                    if(dataUpdate == null)
+                    {
+                        result.IsError = true;
+                        result.Message = "Câu hỏi không được bỏ trống!";
+                        return result;
+                    }
+                    if (string.IsNullOrEmpty(dataUpdate.Question))
+                    {
+                        result.IsError = true;
+                        result.Message = "Câu hỏi không được bỏ trống!";
+                        return result;
+                    }
+                    if (string.IsNullOrEmpty(dataUpdate.Answer))
+                    {
+                        result.IsError = true;
+                        result.Message = "Câu trả lời không được bỏ trống!";
+                        return result;
+                    }
+                    var admin = db.tbl_Accounts.FirstOrDefault(x => x.Username == createdBy.ToLower());
+                    if (admin != null)
+                    {
+                        var data = db.tbl_Questions.FirstOrDefault(x => x.ID == dataUpdate.ID);
+                        data.Question = dataUpdate.Question;
+                        data.Answer = dataUpdate.Answer;
+                        data.Status = dataUpdate.Status;
+                        data.ModifiedBy = admin.Username;
+                        data.MidifiedOn = DateTime.Now;
+                        db.Update(data);
+                        int kq = db.SaveChanges();
+                        if (kq > 0)
+                        {
+                            result.IsError = false;
+                            result.Data = true;
+                            result.Message = "Cập nhật thành công!";
+                            return result;
+                        }
+                    }
+                    result.IsError = true;
+                    result.Message = "Hệ thống thực thi không thành công. Vui lòng thử lại!";
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error("Lỗi cập nhật câu hỏi", ex.Message);
+                result.IsError = true;
+                result.Message = "Hệ thống thực thi không thành công. Vui lòng thử lại!";
+                return result;
+            }
+        }
+        public static DataReturnModel<bool> Delete(int id, string createdBy)
+        {
+            DataReturnModel<bool> result = new();
+            try
+            {
+                using (var db = new nhanshiphangContext())
+                {
+                    var data = db.tbl_Questions.FirstOrDefault(x => x.ID == id);
+                    if (data == null)
+                    {
+                        result.IsError = true;
+                        result.Message = "Không tìm thấy thông tin câu hỏi. Vui lòng thử lại!";
+                    }
+                    var admin = db.tbl_Accounts.FirstOrDefault(x => x.Username == createdBy.ToLower());
+                    if (admin != null)
+                    {
+                        data.Status = 2;
+                        data.ModifiedBy = admin.Username;
+                        data.MidifiedOn = DateTime.Now;
+                        db.Update(data);
+                        int kq = db.SaveChanges();
+                        if (kq > 0)
+                        {
+                            result.IsError = false;
+                            result.Data = true;
+                            result.Message = "Xóa thành công!";
+                            return result;
+                        }
+                    }
+                    result.IsError = true;
+                    result.Message = "Hệ thống thực thi không thành công. Vui lòng thử lại!";
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error("Lỗi xóa câu hỏi", ex.Message);
+                result.IsError = true;
+                result.Message = "Hệ thống thực thi không thành công. Vui lòng thử lại!";
+                return result;
+            }
+        }
+
+        public static tbl_Questions? GetByID(int id)
+        {
+            using (var db = new nhanshiphangContext())
+            {
+                return db.tbl_Questions.FirstOrDefault(x => x.ID == id);
+            }
+        }
+    }
+}
