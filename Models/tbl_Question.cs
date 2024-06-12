2113ad88d6c45777bc42bc5d9106b929f98cdc11
nvhaohg <nvhaohg95@gmail.com>
2024-06-13 00:01:52 +0700

    1

---
+++ Models/tbl_Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KGQT.Models
+{
+    public partial class tbl_Question
+    {
+        public int ID { get; set; }
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
+        public int? Status { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? MidifiedOn { get; set; }
+    }
+}
